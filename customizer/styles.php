4bfd61b09e43b42ac4ab9ad8bdd25efd8f206860
Fabien Quatravaux <fabien.quatravaux@1nterval.com>
2013-04-12 15:58:06 +0200

    [dev] Integrate the background image customizer

--- customizer/styles.php
+++ customizer/styles.php
@@ -5,6 +5,10 @@
  */
 function artsite_background_css() {
   $color = get_theme_mod( 'artsite_background_color' );
+  $bg_image = get_theme_mod('background_image');
+  $bg_repeat = get_theme_mod('background_repeat');
+  $bg_position_x = get_theme_mod('background_position_x');
+  $bg_attachment = get_theme_mod('background_attachment');
 
   // Make sure colors are properly formatted
   $color = '#' . str_replace( '#', '', $color );
@@ -12,7 +16,7 @@ function artsite_background_css() {
 
   <style>
     body.custom-background, body{ background-color: <?php echo $color; ?>; }
-    #wrap{ background: <?php echo $color; ?>; }
+    #wrap{ background: <?php echo "$color url($bg_image) $bg_repeat $bg_position_x $bg_attachment" ?>; }
   </style>
   <?php
 }
